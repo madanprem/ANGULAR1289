376a6bf8452e7aff24d9d175e0314ffeec0110a0
madanprem <madanpremkumar8@gmail.com>
2020-01-24 12:04:40 +0530

    done

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path =require('path')
+const app = express()
+const port = process.env.port || 3000 //process.env.port for heroku
+app.use(express.static(__dirname + '/dist/ngproject'));//gets content like jscript an dother from ngproject 
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/ngproject/index.html')))//backend route
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
